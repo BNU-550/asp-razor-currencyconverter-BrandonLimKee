26102c5633e16a9dfecd43063c9545471efa2285
BrandonLimKee <78452249+BrandonLimKee@users.noreply.github.com>
2021-10-14 17:32:23 +0100

    Added Converter page

---
+++ Pages/CurrencyConverter.cshtml.cs
@@ -0,0 +1,185 @@
+using Microsoft.AspNetCore.Mvc;
+
+
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+
+using Microsoft.Extensions.Logging;
+
+
+
+using System;
+
+
+
+using System.Collections.Generic;
+
+
+
+using System.Linq;
+
+
+
+using System.Threading.Tasks;
+
+
+
+
+
+
+
+namespace CurrencyConverter.Pages
+
+
+
+{
+
+
+
+    public class CurrencyConverterModel : PageModel
+
+
+
+    {
+
+
+
+        public string FullName = "Brandon";
+
+
+
+
+
+
+
+        [BindProperty]
+
+
+
+        public Double GBP { get; set; }
+
+
+
+
+
+
+
+        public const Double Dollar_Exchange = 1.3665346;
+
+
+
+        [BindProperty]
+
+
+
+        public Double Dollar { get; set; }
+
+
+
+
+
+
+
+        private readonly ILogger<CurrencyConverterModel> _logger;
+
+
+
+
+
+
+
+        public CurrencyConverterModel(ILogger<CurrencyConverterModel> logger)
+
+
+
+        {
+
+
+
+            _logger = logger;
+
+
+
+        }
+
+
+
+
+
+
+
+        public void OnGet()
+
+
+
+        {
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+        public void OnPost()
+
+
+
+        {
+
+
+
+            if (GBP < 1)
+
+
+
+            {
+
+
+
+                ViewData["Message"] = Dollar_Exchange;
+
+
+
+            }
+
+
+
+            else
+
+
+
+            {
+
+
+
+                Dollar = GBP * Dollar_Exchange;
+
+
+
+                ViewData["Message"] = Math.Round(Dollar, 2);
+
+
+
+            }
+
+
+
+        }
+
+
+
+    }
+
+
+
+}
